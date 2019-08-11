5b365823bd1ca16b06c0d8753f10636a935ee9dd
Abhisek Pattnaik <abhisekp@engineer.com>
2019-08-11 22:35:47 +0530

    Add travis badge and doc

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # findMinMaxSubArray
+
+> Find the largest/minimum combination of 3 contiguous numbers with maximum sum in an array
+
+[![Build Status](https://travis-ci.org/abhisekp/findMinMaxSubArray.svg?branch=master)](https://travis-ci.org/abhisekp/findMinMaxSubArray)
--- jest.config.js
+++ jest.config.js
@@ -18,13 +18,13 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: null,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -32,12 +32,7 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: null,
@@ -129,7 +124,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
